73a67f212a2d8d17de7dc87a8a5d1ed389cc3d14
thiernodiallo222 <thiernodiallo864@gmail.com>
2020-07-23 18:42:46 -0400

    setting up the framework

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="main-component">
-      <div className='grid-component'>
 
+      
+      <div className='top-buttons-wrapper'>
+        <button className='all-buttons'>Button-One</button>
+        <button className='all-buttons'>Button-two</button>
+        <button className='all-buttons'>Button-three</button>
       </div>
-  
+
+      <div className='grid-wrapper'>
+        </div>
+      
+      <div className='bottom-buttons-wrapper'>
+         <button className='all-buttons'>Button-Four</button>
+        <button className='all-buttons'>Button-Five</button>
+        <button className='all-buttons'>Button-Six</button>
+      </div>
+      
     </div>
-  );
+  )
+
 }
 
 export default App;
