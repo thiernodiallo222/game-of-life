c0f4e790a25715a21cebb0c0d6ff64579cb490fb
thiernodiallo222 <thiernodiallo864@gmail.com>
2020-07-28 16:45:47 -0400

    was able to display the grid and select cells

--- src/App.js
+++ src/App.js
@@ -3,37 +3,83 @@ import './App.css';
 import Grid from './Grid';
 
 
-export class App extends React.Component {
-  constructor(){
+class App extends React.Component {
+  selectCell = (row, col) => {
+    let copy = clonedGrid(this.state.fullGrid);
+    copy[row][col] = !copy[row][col];
+    this.setState({
+      fullGrid: copy,
+    })
+  }
+
+  seed = () => {
+    let copy = clonedGrid(this.state.fullGrid);
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        if (Math.floor(Math.random() * 4)=== 1) {
+          copy[i][j] = true;
+        }
+      }
+    }
+     this.setState({
+      fullGrid: copy,
+    })
+  }
+  startButton = () => {
+    clearInterval(this.IntervalId);
+    this.IntervalId = setInterval(this.play, this.speed);
+  }
+  play = () => {
+    let g = this.state.fullGrid;
+    let g2 = this.clonedArray(this.state.fullGrid);
+
+    
+  }
+  componentDidMount() {
+    this.seed()
+    this.startButton();
+  }
+  constructor() {
     super();
+    this.speed = 100;
+    this.rows = 30;
+    this.cols = 50;
+
     this.state = {
-      generation:0
+      generation: 0,
+      fullGrid: Array(this.rows).fill().map(() => Array(this.cols).fill(false)),
     }
   }
+  
   render() {
     return (
-      <div className="main-component">
-      <div className='top-buttons-wrapper'>
-        <button className='all-buttons'>START / STOP</button>
-        <button className='all-buttons'>GRID SIZING</button>
-        <button className='all-buttons'>CUSTOM PATTERN</button>
-      </div>
+      <div>
+        <h2 className="game-title">Game of Life</h2>
 
-        <div className='grid-wrapper'>
-          <Grid className='grid'/>
+        <div className="buttons-wrapper">
+
+          <button className='all-buttons'>START / STOP</button>
+          <button className='all-buttons'>GRID SIZING</button>
+          <button className='all-buttons'>CUSTOM PATTERN</button>
+           <button className='all-buttons'>RANDOM PATTERN</button>
+          <button className='all-buttons'>CLEAR ALL</button>
         </div>
-      
-      <div className='bottom-buttons-wrapper'>
-         <div className='all-buttons' generation-div><h3 >GENERATION: {this.state.generation}</h3></div>
-        <button className='all-buttons'>RANDOM PATTERN</button>
-        <button className='all-buttons'>CLEAR ALL</button>
-      </div>
-      
-    </div>
-  )
+
+     
+          <Grid className="grid"
+            fullGrid={this.state.fullGrid}
+            rows={this.rows}
+            cols={this.cols}
+            selectCell={this.selectCell}
+          />
+       
+        <h2 className="generation">Generation: {this.state.generation}</h2>
+        </div>
+    );
   }
 }
-
+function clonedGrid(grid) {
+  return JSON.parse(JSON.stringify(grid));
+}
 export default App
 
-// this is just a comment
--- src/Grid.js
+++ src/Grid.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react'
+import Cell from './Cell'
+import "./App.css";
+class Grid extends Component {
 
-export class Grid extends Component {
-  constructor() {
-    super()
-    this.state = {
-      stuff: 23, 
-      anotherstuff: 87
-
-    }
-  }
   render() {
+    const width = this.props.cols*17;
+    const rowsArray = [];
+    let cellClass = "";
+    for (let i = 0; i < this.props.rows; i++){
+      for (let j = 0; j < this.props.cols; j++){
+        let cellId = i + "_" + j;
+        cellClass = this.props.fullGrid[i][j] ? "cell on" : "cell off";
+        rowsArray.push(
+          <Cell
+            cellClass={cellClass}
+            key={cellId}
+            cellId={cellId}
+            row={i}
+            col={j}
+            selectCell={this.props.selectCell}
+          />
+        );
+       }
+    }
     return (
-      <div>
-        <h3>I am the grid</h3>
-        
+      <div className='grid' style={{width: width}}>
+       {rowsArray}
       </div>
     )
   }
